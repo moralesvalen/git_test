c77955f9349cbe349c1fe1bd2c3494290ace14bc
Morales <Jose.Morales@smith-nephew.com>
2025-03-27 15:35:36 -0400

    Anagrama half

--- algoritmos_4_6.js
+++ algoritmos_4_6.js
@@ -347,3 +347,120 @@ console.log(emojifyWord("elephant"));
 
 console.log(emojifyPhrase("I :heart: my :cat:"));
 console.log(emojifyPhrase("I :heart: my :elephant:"));
+
+/* 
+
+Anagrams are groups of words that can be spelled with the same letters. 
+For example, the letters in "pea" can be rearrange to spell "ape", and 
+the letters in "allergy" can be rearranged to spell "gallery."
+
+Write a function to check if two strings of lowercase letters are anagrams. 
+Return true if the word is an anagram. Return false if it isn't. 
+
+Example input: "allergy", "gallery"
+Example output: true
+
+Example input: "rainbow", "crossbow"
+Example output: false
+
+Los anagramas son grupos de palabras que se pueden formar con las mismas letras. 
+Por ejemplo, las letras en "pea" se pueden reorganizar para formar "ape", y las 
+letras en "allergy" se pueden reorganizar para formar "gallery".
+
+Escribe una función para verificar si dos cadenas de letras minúsculas son anagramas. 
+Devuelve true si la palabra es un anagrama. Devuelve false si no lo es.
+
+Ejemplo de entrada: "allergy", "gallery" Ejemplo de salida: true
+
+Ejemplo de entrada: "rainbow", "crossbow" Ejemplo de salida: false
+
+*/ 
+
+function isAnagram(str1, str2){
+  arr1 = str1.split("");
+  arr2 = str2.split("");
+  if (arr1.length === arr2.length){
+    let out = arr1.some(letter => arr2.includes(letter));
+    return out;
+
+  }else{
+    return false
+  }
+
+}
+
+console.log(isAnagram("allergy", "gallery"));
+console.log(isAnagram("treasure", "measure"));
+console.log(isAnagram("sapo", "pasa"));
+
+
+/* We Come in Peace!   
+We've received what (we assume) is a message of peace and brotherhood from 
+an alien planet. They almost got it right, but the messages are 
+backward. Write functions to reverse the backward messages so we can 
+read what they have to say! 
+*/ 
+
+const title = ":htraE no od ot ffutS";
+const messages = [
+            "maerc eci yrT",
+            "rewoT leffiE tisiV",
+            "noom eht ot snamuh etacoleR",
+            "egrahc ni stac tuP", 
+        ]
+
+/* Step 1: Reverse a string
+Write a function that takes in a string and returns the reverse 
+of that string. An interviewer may want to check if you know your
+string methods, or may want to know if you can reverse a string manually. 
+Practice both ways! 
+
+Example input: !htrae ot emocleW
+Example output: Welcome to earth!
+*/  
+
+function reverseString(str){
+    return str;
+}
+
+/*
+Step 2: Now we'll reverse all strings in an array. Write a function that takes in
+an array of strings and returns a new array with all strings reversed.
+
+You can use reuse your reverseString() function, use string methods, or 
+reverse the strings manually. 
+*/ 
+
+function reverseStringsInArray(arr){
+    return arr;
+}
+
+console.log(reverseString(title));
+console.log(reverseStringsInArray(messages));
+
+
+/*  
+Palindromes are words that are the same forward or backward. For example, 
+the words "noon" and "kayak" are a palindromes.
+ 
+Write a function to check if a lowercased string of letters is a palindrome. 
+If the word is palindrome, return true. If it isn't, return false.
+
+Example input: "motorbike"
+Example output: false
+
+Example input: "rotator" 
+Example output: true
+*/
+
+// Solution 1: for loop 
+function isPalindrome(str){
+
+}
+
+// Test your function
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
